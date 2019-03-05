7dfad0af3f8e784d34c984162e7887775e5f6f4d
yaoyizhou1 <yauri.zhang@qunar.com>
2019-03-05 21:43:22 +0800

    初始

--- src/main/java/cn/gaoyuan/demo/algorithm/stack/MyStackTest.java
+++ src/main/java/cn/gaoyuan/demo/algorithm/stack/MyStackTest.java
@@ -15,21 +15,6 @@ import java.util.Arrays;
 public class MyStackTest {
 
     @Test
-    public void myQuene() {
-        MyQuene quene = new MyQuene();
-        quene.add(3);
-        quene.add(4);
-        quene.add(5);
-
-        System.out.println(quene.poll());
-        System.out.println(quene.poll());
-        System.out.println(quene.poll());
-
-        System.out.println(quene.isEmpty());
-
-    }
-
-    @Test
     public void mystack() {
         MyStack stack = new MyStack();
         stack.push(1);
@@ -42,36 +27,5 @@ public class MyStackTest {
         System.out.println(pop1);
     }
 
-    @Test
-    public void array() {
-        int[] a = new int[]{1, 2, 3};
 
-        int[] b = new int[4];
-
-        a = b;
-        a[3] = 4;
-
-        System.out.println(Arrays.toString(a));
-
-    }
-
-    @Test
-    public void addValue() {
-
-        int[] a = new int[]{1, 2, 3, 4};
-
-        int[] b = new int[a.length - 1];
-
-        int flag = 2;
-
-        for (int i = 0; i < a.length - 1; i++) {
-            if (i < flag) {
-                b[i] = a[i];
-            } else {
-                b[i] = a[i + 1];
-            }
-        }
-
-        System.out.println(Arrays.toString(b));
-    }
 }
