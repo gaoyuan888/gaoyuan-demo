c4f833987eb87d2cb1d7ac82665c7d86919e3bb0
yaoyizhou1 <yauri.zhang@qunar.com>
2019-03-05 13:32:00 +0800

    初始

--- src/main/java/cn/gaoyuan/demo/algorithm/SearchTest.java
+++ src/main/java/cn/gaoyuan/demo/algorithm/SearchTest.java
@@ -13,9 +13,32 @@ public class SearchTest {
 
     //    二分查找
     @Test
-    public void two() {
+    public void binarySearch() {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 8, 9};
-
+        //目标
+        int target = 1;
+        //开始位置
+        int begin = 0;
+        //开始位置
+        int end = arr.length - 1;
+        //中间位置
+        int mid = (begin + end) / 2;
+        while (true) {
+            if (end < begin) {
+                throw new RuntimeException("不存在");
+            }
+            if (arr[mid] == target) {
+                System.out.println(arr[mid]);
+                break;
+            } else {
+                if (arr[mid] > target) {
+                    end = mid - 1;
+                } else if (arr[mid] < target) {
+                    begin = mid + 1;
+                }
+                mid = (begin + end) / 2;
+            }
+        }
 
     }
 
