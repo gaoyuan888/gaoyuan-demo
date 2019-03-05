d3eb8544e8f80026e546720a95aefba3d88f343b
yaoyizhou1 <yauri.zhang@qunar.com>
2019-03-05 13:33:37 +0800

    初始

--- src/main/java/cn/gaoyuan/demo/algorithm/MyArray.java
+++ src/main/java/cn/gaoyuan/demo/algorithm/MyArray.java
@@ -1,5 +1,7 @@
 package cn.gaoyuan.demo.algorithm;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 /**
@@ -59,4 +61,37 @@ public class MyArray {
         this.elements = newArry;
 
     }
+
+    //二分查找
+    @Test
+    public void binarySearch() {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 8, 9};
+        //目标
+        int target = 1;
+        //开始位置
+        int begin = 0;
+        //开始位置
+        int end = arr.length - 1;
+        //中间位置
+        int mid = (begin + end) / 2;
+        while (true) {
+            if (end < begin) {
+                throw new RuntimeException("不存在");
+            }
+            if (arr[mid] == target) {
+                System.out.println(arr[mid]);
+                break;
+            } else {
+                if (arr[mid] > target) {
+                    end = mid - 1;
+                } else if (arr[mid] < target) {
+                    begin = mid + 1;
+                }
+                mid = (begin + end) / 2;
+            }
+        }
+
+    }
+
+
 }
