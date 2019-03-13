6606f6097231ca89ef4beb09d7f6608e056d43ee
yaoyizhou1 <yauri.zhang@qunar.com>
2019-03-13 18:50:52 +0800

    初始

--- src/main/java/cn/gaoyuan/demo/algorithm/tree/TreeNodeTest.java
+++ src/main/java/cn/gaoyuan/demo/algorithm/tree/TreeNodeTest.java
@@ -14,11 +14,15 @@ public class TreeNodeTest {
     @Test
     public void test() {
         BinaryTree binaryTree = new BinaryTree();
+        //根节点
         TreeNode node = new TreeNode(1);
+        //左节点
         TreeNode left = new TreeNode(2);
+        //右节点
         TreeNode right = new TreeNode(3);
         node.setLeft(left);
         node.setRight(right);
+        //设置根节点
         binaryTree.setRoot(node);
     }
 }
