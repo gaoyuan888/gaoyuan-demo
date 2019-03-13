5d0d5af9c8dc30c15242a2a61e48c153f7b64dbf
yaoyizhou1 <yauri.zhang@qunar.com>
2019-03-13 12:59:04 +0800

    初始

---
+++ src/main/java/cn/gaoyuan/demo/algorithm/sort/radix/byquene/Quene.java
@@ -0,0 +1,43 @@
+package cn.gaoyuan.demo.algorithm.sort.radix.byquene;
+
+/**
+ * 功能描述:队列
+ *
+ * @author yaoyizhou
+ * @date 2019/3/5 21:13
+ * @desc
+ */
+public class Quene {
+    int[] elements;
+
+    public Quene() {
+        elements = new int[0];
+    }
+
+    //判断是否为空
+    public boolean isEmpty() {
+        return elements.length == 0;
+    }
+
+
+    //入队
+    public void add(int element) {
+        int[] newArray = new int[elements.length + 1];
+        for (int i = 0; i < elements.length; i++) {
+            newArray[i] = elements[i];
+        }
+        newArray[elements.length] = element;
+        elements = newArray;
+    }
+
+    //出队
+    public int poll() {
+        int element = elements[0];
+        int[] newArray = new int[elements.length - 1];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = elements[i + 1];
+        }
+        elements = newArray;
+        return element;
+    }
+}
---
+++ src/main/java/cn/gaoyuan/demo/algorithm/sort/radix/byquene/RadixSort.java
@@ -0,0 +1,56 @@
+package cn.gaoyuan.demo.algorithm.sort.radix.byquene;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 功能描述:基数排序
+ *
+ * @author yaoyizhou
+ * @date 2019/3/12 13:29
+ * @desc
+ */
+public class RadixSort {
+
+    @Test
+    public void test() {
+        int[] arr = new int[]{12, 3654, 13335, 3232, 42, 6, 234, 3, 5321, 6, 3};
+        System.out.println(Arrays.toString(arr));
+        redixSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public void redixSort(int[] arr) {
+        //确定最大位数
+        Integer max = Integer.MIN_VALUE;
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        //最大值的位数就是需要循环的次數
+        int maxLengh = (max + "").length();
+        for (int i = 0, n = 1; i < maxLengh; i++, n *= 10) {
+            //用队列接收
+            Quene[] quenes = new Quene[10];
+            for (int l = 0; l <  quenes.length; l++) {
+                quenes[l] =  new Quene();
+            }
+            //按基数将值放到临时数组中
+            for (int j = 0; j < arr.length; j++) {
+                //按照基数求余数
+                int ys = (arr[j] / n) % 10;
+                //赋值临时队列
+                quenes[ys].add(arr[j]);
+            }
+            //将临时数组中的值，拷贝到原数组
+            int index = 0;
+            for (int k = 0; k < quenes.length; k++) {
+                while (!quenes[k].isEmpty()) {
+                    arr[index++] = quenes[k].poll();
+                }
+            }
+        }
+    }
+}
