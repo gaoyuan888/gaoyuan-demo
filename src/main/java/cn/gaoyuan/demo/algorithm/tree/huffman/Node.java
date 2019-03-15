780e9f7872df1eed80bfa81c3a063c92145db746
yaoyizhou1 <yauri.zhang@qunar.com>
2019-03-15 21:48:05 +0800

    初始

---
+++ src/main/java/cn/gaoyuan/demo/algorithm/tree/huffman/Node.java
@@ -0,0 +1,19 @@
+package cn.gaoyuan.demo.algorithm.tree.huffman;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2019/3/15 21:36
+ * @desc
+ */
+public class Node {
+
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/cn/gaoyuan/demo/algorithm/tree/huffman/TestHuffman.java
@@ -0,0 +1,51 @@
+package cn.gaoyuan.demo.algorithm.tree.huffman;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2019/3/15 21:36
+ * @desc
+ */
+public class TestHuffman {
+
+    @Test
+    public void test() {
+        int[] arr = {3, 5, 7, 8, 11, 23, 14, 29};
+
+        Node huffmanTree = createHuffmanTree(arr);
+    }
+
+    /**
+     * 创建霍夫曼树
+     *
+     * @param arr
+     * @return
+     */
+    public Node createHuffmanTree(int[] arr) {
+
+        //先使用数组中所有的元素创建若干个二叉树（只有一个节点）
+        List<Node> nodes = new ArrayList<>();
+        for (int value : arr) {
+            nodes.add(new Node(value));
+        }
+        //排序
+
+
+
+        //取出来权值最小的两个二叉树
+
+        //创建一颗新的二叉树
+
+        //把取出来的两个二叉树移除
+
+        //放入原来的二叉树集合中
+
+
+    }
+}
