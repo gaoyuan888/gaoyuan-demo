32e0b5f817e077d92cf55dafa6ec73cf6b75b258
yaoyizhou1 <yauri.zhang@qunar.com>
2019-04-22 20:13:39 +0800

    初始

--- src/main/java/cn/gaoyuan/demo/algorithm/tree/huffman/demo1/TestHuffmanCode.java
+++ src/main/java/cn/gaoyuan/demo/algorithm/tree/huffman/demo1/TestHuffmanCode.java
@@ -1,6 +1,5 @@
 package cn.gaoyuan.demo.algorithm.tree.huffman.demo1;
 
-import java.io.StringWriter;
 import java.util.*;
 
 public class TestHuffmanCode {
@@ -10,7 +9,8 @@ public class TestHuffmanCode {
         byte[] bytes = msg.getBytes();
         //进行霍夫曼编码
         byte[] b = huffmanCode(bytes);
-
+        System.out.println(bytes.length);
+        System.out.println(b.length);
     }
 
     /**
@@ -25,19 +25,75 @@ public class TestHuffmanCode {
         //创建一棵霍夫曼树
         Node tree = createHuffmanTree(nodes);
         //创建一颗霍夫曼编码表出来
-        Map<Byte,String> huffCodes=getCodes(tree);
+        Map<Byte, String> huffCodes = getCodes(tree);
         //编码
-        return null;
+        byte[] b = zip(huffCodes, bytes);
+        return b;
     }
 
-    static StringBuilder sb=new StringBuilder();
-    private static Map<Byte,String> getCodes(Node tree) {
+    /**
+     * 霍夫曼编码
+     *
+     * @param huffCodes
+     * @param bytes
+     * @return
+     */
+    private static byte[] zip(Map<Byte, String> huffCodes, byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        //把需要压缩的byte数组处理成一个二进制的字符串
+        for (byte b : bytes) {
+            sb.append(huffCodes.get(b));
+        }
+        //定义长度
+        int len;
+        if (sb.length() % 8 == 0) {
+            len = sb.length() / 8;
+        } else {
+            len = sb.length() / 8 + 1;
+        }
+        //用于存储压缩后的byte
+        byte[] by = new byte[len];
+        //重新定义一个标记
+        int index = 0;
+        for (int i = 0; i < sb.length(); i += 8) {
+            String strByte;
+            if (i + 8 > sb.length()) {
+                strByte = sb.substring(i);
+            } else {
+                strByte = sb.substring(i, i + 8);
+            }
+            byte byt = (byte) Integer.parseInt(strByte, 2);
+            by[index++]=byt;
+            System.out.println(strByte);
+        }
+        return by;
+    }
+
+    //用于临时存储路径
+    static StringBuilder sb = new StringBuilder();
+    //用于临时存储霍夫曼编码
+    static Map<Byte, String> huffCodes = new HashMap<>();
+
+    //根据霍夫曼树获取霍夫曼编码
+    private static Map<Byte, String> getCodes(Node tree) {
         //递归
-        if(tree==null){
+        if (tree == null) {
             return null;
         }
+        getCodes(tree.left, "0", sb);
+        getCodes(tree.right, "1", sb);
+        return huffCodes;
+    }
 
-
+    private static void getCodes(Node node, String s, StringBuilder sb) {
+        StringBuilder sb2 = new StringBuilder(sb);
+        sb2.append(s);
+        if (node.data == null) {
+            getCodes(node.left, "0", sb2);
+            getCodes(node.right, "1", sb2);
+        } else {
+            huffCodes.put(node.data, sb2.toString());
+        }
     }
 
     /**
