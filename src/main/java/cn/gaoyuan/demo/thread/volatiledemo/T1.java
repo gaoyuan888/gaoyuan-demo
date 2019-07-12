8b6558480a44eb8bd07fae4d6f66ecfd31240690
yaoyizhou1 <yauri.zhang@qunar.com>
2019-07-12 17:04:50 +0800

    初始

--- src/main/java/cn/gaoyuan/demo/thread/volatiledemo/T1.java
+++ src/main/java/cn/gaoyuan/demo/thread/volatiledemo/T1.java
@@ -1,5 +1,8 @@
 package cn.gaoyuan.demo.thread.volatiledemo;
 
+/**
+ * 通过javap 命令 查看汇编指令n++是实际分为3个步骤的，非原子性操作，因此可能存在并发问题
+ */
 public class T1 {
 
     volatile int n = 0;
@@ -7,7 +10,8 @@ public class T1 {
     public static void main(String[] args) {
 
     }
-    public  void add() {
+
+    public void add() {
         n++;
     }
 }
