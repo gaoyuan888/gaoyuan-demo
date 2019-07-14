9fe1f9aa9596ed543d27d831e24d9988b13c39ba
wukong.sun <wukong.sun@qunar.com>
2019-07-14 21:32:28 +0800

    Merge branch 'master' of https://github.com/gaoyuan888/gaoyuan-demo
    # Conflicts:
    #	src/main/java/cn/gaoyuan/demo/algorithm/sort/SelectSort.java

--- src/main/java/cn/gaoyuan/demo/thread/container/ContainerNotSafeDemo.java
+++ src/main/java/cn/gaoyuan/demo/thread/container/ContainerNotSafeDemo.java
@@ -1,8 +1,7 @@
 package cn.gaoyuan.demo.thread.container;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,15 +11,46 @@ import java.util.concurrent.Executors;
 public class ContainerNotSafeDemo {
 
     public static void main(String[] args) {
-        final List<String> list = new ArrayList<>();
+//       listNotSafe();
+
+        List<String> list = new ArrayList<>();
         ExecutorService executorService = Executors.newCachedThreadPool();
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 300; i++) {
             executorService.execute(() -> {
                 list.add(UUID.randomUUID().toString().substring(0, 8));
                 System.out.println(list);
             });
         }
+    }
+
+    public static void listNotSafe(){
+        List<String> list = new CopyOnWriteArrayList<>();//Collections.synchronizedList(new ArrayList<>());//new Vector<>(); // new ArrayList<>();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 300; i++) {
+            executorService.execute(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            });
+        }
+//      java.util.ConcurrentModificationException
+        /**
+         * 1。故障
+         *    java.util.ConcurrentModificationException
+         * 2.导致原因
+         *   并发争抢修改导致，参考我们的花名册签名情况。
+         *   一个人正在写，另外一个同学过来抢夺，导致数据不一致的异常，并发修改异常。
+         * 3.解决方案
+         *   3.1 new Vector<>();
+         *   3.2 Collections.synchronizedList(new ArrayList<>());
+         *   3.3 new CopyOnWriteArrayList<>();
+         *       写时复制-->读写分离思想
+         *
+         *
+         * 4.优化建议（同样的错误不犯第二次）
+         */
+
 
     }
 }
