71392f929cae05244e1d9078e201f068ee118fd2
yaoyizhou1 <yauri.zhang@qunar.com>
2019-07-23 17:51:22 +0800

    初始

--- src/main/java/cn/gaoyuan/demo/qa/conditiondemo/ConditionDemo.java
+++ src/main/java/cn/gaoyuan/demo/qa/conditiondemo/ConditionDemo.java
@@ -18,45 +18,99 @@ import java.util.concurrent.locks.ReentrantLock;
  * @desc
  */
 
-public class ConditionDemo {
+class Myshare {
     Lock lock = new ReentrantLock();
     Condition c1 = lock.newCondition();
     Condition c2 = lock.newCondition();
     Condition c3 = lock.newCondition();
+    public volatile Integer number = 0;
 
-    public void printa() {
+    public void print5() {
         lock.lock();
         try {
-            new Thread(() -> {
-                for (int i = 0; i < 5; i++) {
-                    System.out.println("a");
-                }
-            }, "AA").start();
-        } catch (Exception e) {
+            //判断
+            while (number != 0) {
+                c1.await();
+            }
+            //干活
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+            }
+            number = 1;
+            //唤醒
+            c2.signal();
 
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             lock.unlock();
         }
-
-
     }
 
-    public void printb() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("b");
+    public void print10() {
+        lock.lock();
+        try {
+            //判断
+            while (number != 1) {
+                c2.await();
+            }
+            //干活
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+            }
+            number = 2;
+            //唤醒
+            c3.signal();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 
-    public void printc() {
-        for (int i = 0; i < 15; i++) {
-            System.out.println("c");
+    public void print15() {
+        lock.lock();
+        try {
+            //判断
+            while (number != 2) {
+                c3.await();
+            }
+            //干活
+            for (int i = 0; i < 15; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+            }
+            number = 0;
+            //唤醒
+            c1.signal();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
+}
 
 
+public class ConditionDemo {
     public static void main(String[] args) {
-
-
+        Myshare myshare = new Myshare();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                myshare.print5();
+            }
+        }, "AA").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                myshare.print10();
+            }
+        }, "BB").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                myshare.print15();
+            }
+        }, "CC").start();
     }
 
 
