c7d65894864ce0af6c3f9cffc1afb9990b85df03
yaoyizhou1 <yauri.zhang@qunar.com>
2019-07-29 19:39:11 +0800

    初始

---
+++ src/main/java/cn/gaoyuan/demo/qa/gcdemo/HelloGc.java
@@ -0,0 +1,18 @@
+package cn.gaoyuan.demo.qa.gcdemo;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2019/7/29 19:30
+ * @desc
+ */
+public class HelloGc {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("******HelloGc");
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+    }
+
+}
