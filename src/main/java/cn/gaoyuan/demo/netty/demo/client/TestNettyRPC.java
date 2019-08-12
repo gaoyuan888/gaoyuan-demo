2e47b8238b252d4eca09e390cf3e75784c71d436
yaoyizhou1 <yauri.zhang@qunar.com>
2019-08-12 20:20:25 +0800

    netty

--- src/main/java/cn/gaoyuan/demo/netty/demo/client/TestNettyRPC.java
+++ src/main/java/cn/gaoyuan/demo/netty/demo/client/TestNettyRPC.java
@@ -4,7 +4,7 @@ package cn.gaoyuan.demo.netty.demo.client;
 import cn.gaoyuan.demo.netty.demo.clientStub.NettyRPCProxy;
 
 /**
- * ��������:
+ * 主测试类:
  *
  * @author yaoyizhou
  * @date 2019/8/12 19:49
@@ -13,11 +13,11 @@ import cn.gaoyuan.demo.netty.demo.clientStub.NettyRPCProxy;
 public class TestNettyRPC {
     public static void main(String [] args){
 
-        //��1��Զ�̵���
+        //1.第一次远程调用
         HelloNetty helloNetty=(HelloNetty) NettyRPCProxy.create(HelloNetty.class);
         System.out.println(helloNetty.hello());
 
-        //��2��Զ�̵���
+        //1.第二次远程调用
         HelloRPC helloRPC =  (HelloRPC) NettyRPCProxy.create(HelloRPC.class);
         System.out.println(helloRPC.hello("RPC"));
 
