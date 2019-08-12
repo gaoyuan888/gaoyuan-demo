3b96ddf1ace240106d2e1c5667e8f08f40635a4a
yaoyizhou1 <yauri.zhang@qunar.com>
2019-08-12 11:45:38 +0800

    netty

--- src/main/java/cn/gaoyuan/demo/netty/basic/NettyClientHandler.java
+++ src/main/java/cn/gaoyuan/demo/netty/basic/NettyClientHandler.java
@@ -1,7 +1,25 @@
 package cn.gaoyuan.demo.netty.basic;
 
-//客户端业务处理类
-public class NettyClientHandler {
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 客户端业务处理类
+ */
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 通道就绪事件
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("老板还钱吧", CharsetUtil.UTF_8));
+    }
 
 
 }
