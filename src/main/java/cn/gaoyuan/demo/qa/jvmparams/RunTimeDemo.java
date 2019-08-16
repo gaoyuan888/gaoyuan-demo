d38da2b3728c86b00c4211544445d818570a17db
yaoyizhou1 <yauri.zhang@qunar.com>
2019-08-16 19:58:03 +0800

    jvm

---
+++ src/main/java/cn/gaoyuan/demo/qa/jvmparams/RunTimeDemo.java
@@ -0,0 +1,19 @@
+package cn.gaoyuan.demo.qa.jvmparams;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2019/8/16 19:37
+ * @desc
+ */
+public class RunTimeDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().maxMemory() / 1024 / 1024);
+//        java.lang.OutOfMemoryError: Java heap space
+        byte[] bytes=new byte[1024*1024*1500];
+        System.out.println("good 11111");
+        bytes.toString();
+    }
+}
