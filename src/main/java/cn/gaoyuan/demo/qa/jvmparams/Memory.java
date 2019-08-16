8c046cdf8d4762fb9080ac1e97351d2cc48a1fdd
yaoyizhou1 <yauri.zhang@qunar.com>
2019-08-16 13:48:47 +0800

    jvm

---
+++ src/main/java/cn/gaoyuan/demo/qa/jvmparams/Memory.java
@@ -0,0 +1,21 @@
+package cn.gaoyuan.demo.qa.jvmparams;
+
+/**
+ * 功能描述:
+ * <p>
+ * 确定内存区域
+ *
+ * @author yaoyizhou
+ * @date 2019/8/16 13:44
+ * @desc
+ */
+public class Memory {
+
+    public static void main(String[] args) {
+        int i = 1;
+        Object o = new Object();
+        Memory memory = new Memory();
+
+    }
+
+}
