1847d42e85e6ba53ccab0a6429d37062db710fe6
yaoyizhou1 <yauri.zhang@qunar.com>
2019-08-27 15:05:44 +0800

    jvm

--- src/main/java/cn/gaoyuan/demo/qa/jvmparams/RunTimeDemo.java
+++ src/main/java/cn/gaoyuan/demo/qa/jvmparams/RunTimeDemo.java
@@ -10,6 +10,10 @@ package cn.gaoyuan.demo.qa.jvmparams;
 public class RunTimeDemo {
 
     public static void main(String[] args) {
+        Long ss=1254L;
+        System.out.println(ss.intValue());
+
+
         System.out.println(Runtime.getRuntime().maxMemory() / 1024 / 1024);
 //        java.lang.OutOfMemoryError: Java heap space
         byte[] bytes=new byte[1024*1024*1500];
