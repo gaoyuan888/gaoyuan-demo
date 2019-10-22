905e2b0a86a134d65a6bd3e18fd86d43a230ee6b
yaoyizhou1 <yauri.zhang@qunar.com>
2019-10-22 11:16:20 +0800

    帆布

---
+++ src/main/java/cn/gaoyuan/demo/atguigu/Father.java
@@ -0,0 +1,39 @@
+package cn.gaoyuan.demo.atguigu;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2019/10/22 10:57
+ * @desc
+ */
+public class Father {
+
+    private int i = test();
+
+
+    private static int j = method();
+
+    static {
+        System.out.println("(1)");
+    }
+
+    Father() {
+        System.out.println("(2)");
+    }
+
+    {
+        System.out.println("(3)");
+    }
+
+    private int test() {
+        System.out.println("(4)");
+        return 1;
+    }
+
+    private static int method() {
+        System.out.println("(5)");
+        return 1;
+    }
+
+}
---
+++ src/main/java/cn/gaoyuan/demo/atguigu/Son.java
@@ -0,0 +1,43 @@
+package cn.gaoyuan.demo.atguigu;
+
+/**
+ * 功能描述:
+ *5132 10 6 8 7
+ * @author yaoyizhou
+ * @date 2019/10/22 10:59
+ * @desc
+ */
+public class Son extends Father {
+    private int i = test();
+    private static int j = method();
+
+    static {
+        System.out.println("(6)");
+    }
+
+    Son() {
+        System.out.println("(7)");
+    }
+
+    {
+        System.out.println("(8)");
+    }
+
+    private int test() {
+        System.out.println("(9)");
+        return 1;
+    }
+
+    private static int method() {
+        System.out.println("(10)");
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Son s1 = new Son();
+        System.out.println();
+        Son s2 = new Son();
+    }
+
+
+}
