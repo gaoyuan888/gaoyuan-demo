c7c1364c4d8638506d94fe92ee4d9a380cd0e51f
yaoyizhou1 <yauri.zhang@qunar.com>
2019-11-02 19:32:01 +0800

    帆布

---
+++ src/main/java/cn/gaoyuan/demo/atguigu/Test1.java
@@ -0,0 +1,59 @@
+package cn.gaoyuan.demo.atguigu;
+
+import java.io.*;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2019/11/2 19:21
+ * @desc
+ */
+public class Test1 {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(new File("d://a.object")));
+        oos.writeObject(new MyClass(1,2));
+
+        ObjectInputStream ois=new ObjectInputStream(new FileInputStream(new File("d://a.object")));
+        MyClass o = (MyClass)ois.readObject();
+        System.out.println(o);
+
+    }
+}
+
+class MyClass implements Serializable{
+    private int a;
+    private int b;
+
+
+
+    public MyClass(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClass{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
